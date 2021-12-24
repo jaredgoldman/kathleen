8a9b6340bdd3891ea19aa81bcd233a37205825ac
jaredgoldman <jaredgoldmandrums@gmail.com>
2021-12-23 20:39:31 -0500

    correct import statement

--- pages/art.js
+++ pages/art.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 import ArtGallery from '../components/ArtGallery'
 
 export default function art() {
